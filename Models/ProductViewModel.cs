3da91269b6660a4db89eae1f7e8185a47e1d8af9
NikolaDjerov <126071907+NikolaDjerov@users.noreply.github.com>
2026-03-16 22:04:52 +0200

    asp

--- Models/ProductViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Asp_net.Models
-{
-    public class ProductViewModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public decimal Price { get; set; }
-    }
-}
